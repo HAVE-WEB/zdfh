5a4c2f263df75f08d1cc016b266313603b5ea4a5
Administrator <15370036857@sina.cn>
2017-08-03 18:00:02 +0800

    js库

--- assets/js/myFirst.js
+++ assets/js/myFirst.js
@@ -90,29 +90,33 @@ var Out =(function (window, document,$) {
             var num = self.innerWidth;
             if(num <= 375){//iphone6的宽度一下 字体大小为14px
                 $("html").css("font-size","14px");//百分比对象的字体大小为14px，0.875em
-                $("body").css("font-size","1rem");
-                $("input").css("font-size","1rem")
-                $(":submit").css("font-size","1rem");
-                $(":button").css("font-size","1rem");
             }else if(num <= 414){//宽度在iphone6-iphone6s之间
                 $("html").css("font-size","16px");//16px
-                $("body").css("font-size","1rem");
-                $("input").css("font-size","1rem")
-                $(":submit").css("font-size","1rem");
-                $(":button").css("font-size","1rem");
             }else if(num <= 768){
                 $("html").css("font-size","20px");
-                $("body").css("font-size","1rem");
-                $("input").css("font-size","1rem")
-                $(":submit").css("font-size","1rem");
-                $(":button").css("font-size","1rem");
             } else {
-                $("html").css("font-size","30px")
-                $("body").css("font-size","1rem");
-                $("input").css("font-size","1rem")
-                $(":submit").css("font-size","1rem");
-                $(":button").css("font-size","1rem");
+                $("html").css("font-size","30px");
             }
+            $("body").css("font-size","1rem");
+            $("input").css("font-size","1rem");
+            $("input:checkbox").parent().each(function () {
+                $(this).css("font-size","1rem")
+            });
+            $("input:checkbox").each(function () {
+                $(this).css({
+                    width: "1rem",
+                    height: "1rem",
+
+                }).css("margin-right","6px");
+            });
+            $("input:radio").each(function () {
+                $(this).css({
+                    width: "1rem",
+                    height: "1.5rem",
+                });
+            });
+            $(":submit").css("font-size","1rem");
+            $(":button").css("font-size","1rem");
         };
         Rem.adapterWidth_class = function () {//类方法
             var num =window.innerWidth;
@@ -172,7 +176,23 @@ var Out =(function (window, document,$) {
                     $(":submit").css("font-size","1rem");
                     $(":button").css("font-size","1rem");
                 }
-            }
+            },
+            alert_modal: function (message) {
+                var m ='<div class="am-modal am-modal-alert" tabindex="-1" style="border-radius: 1000px" id="my-alert">\n' +
+                    '  <div class="am-modal-dialog">\n' +
+                    '    <div class="am-modal-hd">温馨提示</div>\n' +
+                    '    <div class="am-modal-bd">\n' +
+                    '      '+message+'\n' +
+                    '    </div>\n' +
+                    '    <div class="am-modal-footer">\n' +
+                    '      <span class="am-modal-btn">ok</span>\n' +
+                    '    </div>\n' +
+                    '  </div>\n' +
+                    '</div>';
+                $(".am-modal").remove();
+                $(m).appendTo("body");
+            },
+
     };
     if(typeof module !="undefined" && module.exports){
         module.exports = Rem;
